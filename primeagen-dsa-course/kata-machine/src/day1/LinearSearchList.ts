b4253a7aaba5cdb01f1e43241d4b0204b6f5c7dd
trevorhitv <trevorh@hey.com>
2024-08-19 18:23:29 -0500

    add katas

---
+++ primeagen-dsa-course/kata-machine/src/day1/LinearSearchList.ts
@@ -0,0 +1,9 @@
+export default function linearSearch(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; ++i) {
+        if (haystack[i] === needle) return true;
+    }
+    return false;
+}
