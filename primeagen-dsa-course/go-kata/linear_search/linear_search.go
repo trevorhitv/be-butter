435834d94b076bc5cd13c47a80948359542c4f97
Trevor Huis in 't Veld <trevorh@hey.com>
2024-08-22 16:13:26 -0500

    check in

--- primeagen-dsa-course/go-kata/linear_search/linear_search.go
+++ primeagen-dsa-course/go-kata/linear_search/linear_search.go
@@ -1,5 +1,10 @@
 package linearsearch
 
 func LinearSearch(haystack []int, needle int) bool {
-	return true
+	for item in haystack {
+		if needle == haystack[i] {
+			return true
+		}
+	}
+	return false
 }
--- primeagen-dsa-course/go-kata/linear_search/linear_search_test.go
+++ primeagen-dsa-course/go-kata/linear_search/linear_search_test.go
@@ -1,16 +1,32 @@
 package linearsearch
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestLinearSearch(t *testing.T) {
 	foo := []int{1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420}
 
-	got := LinearSearch(foo, 4)
-	fmt.Printf("result: %t", got)
-	if got != true {
-		t.Errorf("Abs(-1) = %t; want 1", got)
+	if LinearSearch(foo, 4) != true {
+		t.Errorf("wanted true")
 	}
+	if LinearSearch(foo, 69) != true {
+		t.Errorf("wanted true")
+	}
+	if LinearSearch(foo, 1336) != false {
+		t.Errorf("wanted false")
+	}
+	if LinearSearch(foo, 69420) != true {
+		t.Errorf("wanted true")
+	}
+	if LinearSearch(foo, 69421) != false {
+		t.Errorf("wanted false")
+	}
+	if LinearSearch(foo, 1) != true {
+		t.Errorf("wanted true")
+	}
+	if LinearSearch(foo, 0) != false {
+		t.Errorf("wanted false")
+	}
+
 }
