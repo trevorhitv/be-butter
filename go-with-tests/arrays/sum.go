afeb5d3bf9ebcb84618f143bfd8b04d06e91c809
Trevor Huisintveld <trevorh@hey.com>
2024-09-19 18:16:42 -0500

    Update tests and try lazy git

---
+++ go-with-tests/arrays/sum.go
@@ -0,0 +1,14 @@
+package main
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+//  func SumAll(numbersToSum ...int[]) []int {
+//    lengthOfNumbers := len(numbersToSum)
+//    sums := make([]int, lengthOfNumbers)
+//  }
