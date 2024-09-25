f7bb38ef7740ec562316a560a9c535357d248dac
Trevor Huis in 't Veld <trevorh@hey.com>
2024-09-24 20:52:44 -0500

    chapter 4.4

--- lets-go/snippetbox/cmd/web/main.go
+++ lets-go/snippetbox/cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -12,11 +15,19 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", "4000", "HTTP Network Address")
-
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	dsn := flag.String("dsn", "root@tcp(localhost:3306)/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	db.Close()
 
 	app := &application{
 		logger: logger,
@@ -24,7 +35,22 @@ func main() {
 
 	logger.Info("starting server on %d", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
