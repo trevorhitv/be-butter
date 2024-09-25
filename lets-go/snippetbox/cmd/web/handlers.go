2e0a203f40533837895702ecaf662df634ad17df
Trevor Huis in 't Veld <trevorh@hey.com>
2024-09-24 19:16:35 -0500

    finished 4.3

--- lets-go/snippetbox/cmd/web/handlers.go
+++ lets-go/snippetbox/cmd/web/handlers.go
@@ -12,19 +12,18 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	files := []string{
 		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
 	}
+
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		app.serverError(w, r, err)
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 	}
 }
 
